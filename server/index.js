c99370d4467e96ce54f51c64bb1ae91104a5a68b
Basile Raiwet <basileraiwet.pro@gmail.com>
2020-06-25 09:09:19 +0200

    init server

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express();
 const http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
   });
 
 app.listen(8000, () => {
-    console.log('listening on  *:8000')
+    console.log('listening on  http://localhost:8000')
 })
